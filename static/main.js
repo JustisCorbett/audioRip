59746774bc093e6ed1df76b774db02c9f14fc56b
Justis Corbett <corbettjustis@gmail.com>
2021-08-02 18:43:55 -0600

    testing audio player

--- static/main.js
+++ static/main.js
@@ -1,4 +1,5 @@
 async function sendLink() {
+    audioPlayer = document.getElementById("audio-player")
     input = document.getElementById("input-form").value
     data = {link: input}
     const response = await fetch("/get_link", {
@@ -12,6 +13,8 @@ async function sendLink() {
         referrerPolicy: 'no-referrer',
         body: JSON.stringify(data)
     });
+    let blob = new Blob([response.body], {type: response.headers["Content-Type"]});
+    audioPlayer.src = URL.createObjectURL(blob);
     console.log(response);
     return response.json();
 }
