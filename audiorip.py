7eb0aa55035c2f5464601d02a3cb4931bd4a39d2
Justis Corbett <corbettjustis@gmamail.com>
2021-08-05 17:10:15 -0600

    changed file in buffer to temp folder, extractinfo

--- audiorip.py
+++ audiorip.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify, send_file
 import logging
 import magic
 import yt_dlp
+import os
 from contextlib import redirect_stdout
 from io import BytesIO
 
@@ -16,26 +17,29 @@ def get_link():
     data = request.get_json()
     link = data["link"]
     print(link)
-    file = BytesIO()
+    file_info = BytesIO()
     ydl_opts = {
         'format': 'bestaudio/best',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
+            #'preferredcodec': 'mp3',
+            #'preferredquality': '192',
         }],
-        'ffmpeg_location': 'ffmpeg-4.4-essentials_build/bin',
-        'outtmpl': '-',
+        #'ffmpeg_location': 'ffmpeg-4.4-essentials_build/bin',
+        'outtmpl': 'temp/%(title)s.%(ext)s',
         'logger': logging.getLogger(),
-        'quiet': True,
+        #'quiet': True,
     }
-    with redirect_stdout(file):
+    with redirect_stdout(file_info):
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([link])
-    data = {"file": file}
-    file.seek(0)
+            info = ydl.extract_info(link, download=True)
+            filename = ydl.prepare_filename(info)
+    print(filename)
+    for name in os.listdir('/temp'):
+        if filename in name:
+            print (name)
     mime_type = magic.from_buffer(file.read(1024), mime=True)
     file.seek(0)
     print(mime_type)
-    return send_file(file, mimetype=mime_type)
+    return send_file(file)
 #"-o temp/%(title)s.%(ext)s"    '
