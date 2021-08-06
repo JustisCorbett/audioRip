3595323ecc34ffdcfd09352272335f44f59f6ece
Justis Corbett <corbettjustis@gmamail.com>
2021-08-05 18:27:41 -0600

    cleaning imports and comments

--- audiorip.py
+++ audiorip.py
@@ -1,10 +1,7 @@
-from flask import Flask, render_template, request, jsonify, send_file
+from flask import Flask, render_template, request, send_file
 import logging
-import magic
 import yt_dlp
 import os
-from contextlib import redirect_stdout
-from io import BytesIO
 
 app = Flask(__name__)
 
@@ -26,22 +23,16 @@ def get_link():
             #'preferredcodec': 'mp3',
             #'preferredquality': '192',
         }],
-        #'ffmpeg_location': 'ffmpeg-4.4-essentials_build/bin',
         'outtmpl': 'temp/%(title)s.%(ext)s',
         'logger': logging.getLogger(),
-        #'quiet': True,
     }
-    with redirect_stdout(file_info):
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(link, download=True)
-            ext = info.get("ext")
-            filename = ydl.prepare_filename(info)
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(link, download=True)
+        ext = info.get("ext")
+        filename = ydl.prepare_filename(info)
     title = filename.replace(("." + ext), "")
     title = title.replace(("temp/"), "")
     for name in os.listdir(temp_path):
         if title in name:
             found_file = name
-   # mime_type = magic.from_buffer(file.read(1024), mime=True)
-    #file.seek(0)
     return send_file(temp_path + "/" + found_file)
-#"-o temp/%(title)s.%(ext)s"    '
